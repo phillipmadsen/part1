a48ce174a5cf922149cb53f347327ebe3b585f55
phillipmadsen <pmadsen2013@gmail.com>
2016-07-11 12:48:27 -0600

    added morphto relationship
    trying morphto instead of has many

--- app/Models/Image.php
+++ app/Models/Image.php
@@ -14,4 +14,10 @@ class Image extends Model
         'file.mimes' => 'Uploaded file is not in image format',
         'file.required' => 'Image is required'
     ];
+
+
+    public function imageable()
+    {
+        return $this->morphTo();
+    }
 }
--- app/Product.php
+++ app/Product.php
@@ -6,5 +6,12 @@ use Illuminate\Database\Eloquent\Model;
 
 class Product extends Model
 {
-    protected $fillable = ['product_name'];
+    protected $fillable = ['product_name', 'images'];
+
+
+public function images()
+{
+    return $this->morphMany(App\Models\Image::class, 'imageable');
+}
+
 }
