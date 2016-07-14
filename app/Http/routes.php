6db6b32098b32257e159bf01df6b7819adb5f562
Phillip Madsen <pmadsen2013@gmail.com>
2016-07-13 20:53:56 -0600

    update
    files

--- app/Http/routes.php
+++ app/Http/routes.php
@@ -1,5 +1,43 @@
 <?php
 
+require "newRouteCompact.php";
+
+
+Route::get('migrate', function () {\Artisan::call('migrate:refresh'); return '<pre>' . \Artisan::output() . '</pre>'; });
+Route::get('seed', function () {\Artisan::call('db:seed'); return '<pre>' . \Artisan::output() . '</pre>'; });
+Route::get('vc', function () {\Artisan::call('views:clear'); return '<pre>' . \Artisan::output() . '</pre>'; });
+Route::get('env', function () {dd(App::environment()); });
+Route::get('info', function () {if (App::environment() != 'production') {phpinfo(); } });
+Route::get('/rr', function ()
+{
+    function philsroutes()
+    {
+        $routeCollection = Route::getRoutes();
+        echo '<link rel="stylesheet" href="https://maxcdn.bootstrapcdn.com/bootstrap/3.3.6/css/bootstrap.min.css">';
+        echo "<div class='container'><div class='col-md-12'><table class='table table-striped' style='width:100%'>";
+        echo '<tr>';
+        echo "<td width='10%'><h4>HTTP Method</h4></td>";
+        echo "<td width='45%'><h4>Route</h4></td>";
+        echo "<td width='45%'><h4>Corresponding Action</h4></td>";
+        echo '</tr>';
+        foreach ($routeCollection as $value)
+        {
+            echo '<tr>';
+            echo '<td>' . $value->getMethods()[0] . '</td>';
+            echo "<td><a href='" . $value->getPath() . "' target='_blank'>" . $value->getPath() . '</a> </td>';
+            echo '<td>' . $value->getActionName() . '</td>';
+            echo '</tr>';
+        }
+        echo '</table></div></div>';
+        echo '<script src="https://maxcdn.bootstrapcdn.com/bootstrap/3.3.6/js/bootstrap.min.js"></script>';
+    }
+    return philsroutes();
+});
+Route::get('cc', function () {\Artisan::call('cache:clear'); return '<pre>' . \Artisan::output() . '</pre>'; });
+Route::get('rc', function () {\Artisan::call('route:clear'); return '<pre>' . \Artisan::output() . '</pre>'; });
+
+
+
 /*
 |--------------------------------------------------------------------------
 | Application Routes
